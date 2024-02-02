a2f91923c02645f344c1a249ff3319a1f2ffc62c
Anurag Upadhyay <anuragupadhyayblogs@gmail.com>
2024-02-02 15:16:17 +0530

    gateways server

---
+++ apigatewayserver/src/main/java/com/delivery/apigatewayserver/ApigatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayserverApplication.class, args);
+	}
+
+}
---
+++ apigatewayserver/src/test/java/com/delivery/apigatewayserver/ApigatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
